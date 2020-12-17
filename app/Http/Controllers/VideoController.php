577b33d395b5574ae284c921ccf20de526a8792a
Hleb Prakhnitski <thane.crios@gmail.com>
2020-12-17 05:28:21 +0300

    modified S3 saving

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 use Streaming;
 use Streaming\FFMpeg;
 use Streaming\HLSFlag;
+use Streaming\Representation;
 use Throwable;
 
 class VideoController extends Controller
@@ -14,13 +14,14 @@ class VideoController extends Controller
 
     private $ffmpeg;
     private $video;
+    private $bucket;
 
     public function run($pathToFile, $personalBucket = null): ?bool
     {
         try {
             $this->setDriver();
-            $this->initialize($personalBucket, $pathToFile);
-            $this->convert(self::getRandomString() . '/' . self::getRandomString(), true);
+            $this->initialize($pathToFile, $personalBucket);
+            $this->convert(self::getRandomString(12), false, $personalBucket);
             Log::warning("Operation for $personalBucket __ $pathToFile successful!");
             return true;
         } catch (Throwable $th) {
@@ -52,38 +53,54 @@ class VideoController extends Controller
                     'secret' => env('VIDEO_BUCKET_SECRET_KEY'),
                 ]
             ];
-            $bucket = new Streaming\Clouds\S3($bucketConfig);
+            $this->bucket = new Streaming\Clouds\S3($bucketConfig);
             $fileConfig = [
-                'cloud' => $bucket,
+                'cloud' => $this->bucket,
                 'options' => [
                     'Bucket' => $personalBucket,
                     'Key' => $pathToFile
                 ]
             ];
             $this->video = $this->ffmpeg->openFromCloud($fileConfig);
-        } else {
-            $this->video = $this->ffmpeg->open($pathToFile);
+//        } else {
+//            $this->video = $this->ffmpeg->open($pathToFile);
         }
 
     }
 
-    private function convert($saveTo, $saveLocal = false): void
+    private function convert($saveTo, $saveLocal = false, $personalBucket = null): void
     {
-        if ($saveLocal === true) {
-            $url = env('APP_URL') . '/storage/' . $saveTo;
-            $saveTo = storage_path('app/public/' . $saveTo);
-        } else {
-            $url = $saveTo;
-        }
+
+//        if ($saveLocal === true) {
+//            $url = env('APP_URL') . '/storage/' . $saveTo;
+//            $saveTo = storage_path('app/public/' . $saveTo);
+//
+//        } else {
+            $dest = 's3://' . env('VIDEO_SAVE_BUCKET') . '/' . $saveTo;
+            $filename = 'original.m3u8';
+            $to_s3 = [
+                'cloud' => $this->bucket,
+                'options' => [
+                    'dest' => $dest,
+                    'filename' =>  ''
+                ]
+            ];
+            $url = $dest . '/' . $filename;
+//        }
+
+        $r_360p = (new Representation)->setKiloBitrate(276)->setResize(640, 360);
+        $r_480p = (new Representation)->setKiloBitrate(750)->setResize(854, 480);
+        $r_720p = (new Representation)->setKiloBitrate(2048)->setResize(1280, 720);
+
         $this->video->hls()
             ->x264()
             ->fragmentedMP4()
 //            ->setHlsListSize(5)
             ->setFlags([HLSFlag::DELETE_SEGMENTS])
-            ->setHlsTime(30)
+            ->setHlsTime(10)
             ->setHlsAllowCache(false)
-            ->autoGenerateRepresentations()
-            ->save($saveTo);
+            ->addRepresentations([$r_720p])
+            ->save(null, [$to_s3]);
         echo "\n-----\n";
         echo "Url: " . $url;
         echo "\n-----\n";
