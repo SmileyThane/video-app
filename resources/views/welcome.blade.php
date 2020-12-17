ba129d58cce016e70dd473c99851bae89766ddec
Hleb Prakhnitski <thane.crios@gmail.com>
2020-12-17 08:13:29 +0300

    modified view for short video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -399,29 +399,20 @@
 <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 <!-- Or if you want a more recent alpha version -->
 <!-- <script src="https://cdn.jsdelivr.net/npm/hls.js@alpha"></script> -->
-<video id="video" controls="true"></video>
+{{--<video id="video720" controls="true"></video>--}}
+<video id="video480" controls="true"></video>
 <script>
-    var video = document.getElementById('video');
-    var videoSrc = 'https://stagging-nl-dev.s3.amazonaws.com/5fdad4fc6b127/original.m3u8';
+    var video480 = document.getElementById('video480');
+
+
+    var video480Src = 'https://stagging-nl-dev.s3.amazonaws.com/Nwh5bZbKNTmT/original.m3u8';
     if (Hls.isSupported()) {
+
         var hls = new Hls();
-        hls.loadSource(videoSrc);
-        hls.attachMedia(video);
+        hls.loadSource(video480Src);
+        hls.attachMedia(video480);
         hls.showControls();
     }
-        // hls.js is not supported on platforms that do not have Media Source
-        // Extensions (MSE) enabled.
-        //
-        // When the browser has built-in HLS support (check using `canPlayType`),
-        // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
-        // element through the `src` property. This is using the built-in support
-        // of the plain video element, without using hls.js.
-        //
-        // Note: it would be more normal to wait on the 'canplay' event below however
-        // on Safari (where you are most likely to find built-in HLS support) the
-        // video.src URL must be on the user-driven white-list before a 'canplay'
-        // event will be emitted; the last video event that can be reliably
-    // listened-for when the URL is not on the white-list is 'loadedmetadata'.
     else if (video.canPlayType('application/vnd.apple.mpegurl')) {
         video.src = videoSrc;
     }
